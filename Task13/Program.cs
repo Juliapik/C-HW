bfc5dca92dc6c7ea8de477adcf6fbe708d51b0d9
Julia <reboot.3107@gmail.com>
2023-06-04 08:58:22 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n < 100)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (n > 999)
+    {
+        n /= 10;
+    }
+Console.WriteLine(n % 10);
+}
