76554812ed80442a8910eb307b6a66d668ededd3
Julia <reboot.3107@gmail.com>
2023-06-04 09:10:22 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведитете номер дня недели: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n > 5)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
