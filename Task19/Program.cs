f4dd416995f30c38a7994211f10b149be396a61b
Julia <reboot.3107@gmail.com>
2023-06-07 23:34:51 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 10000 || n > 99999)
+{
+    Console.Write("Вы ошиблись!\nпятизначное число: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+int fir = n;
+int sec = 0;
+while (n > 0)
+{
+    sec = sec * 10 + n % 10;
+    n /= 10;
+}
+if (fir == sec)
+    Console.WriteLine("да");
+else 
+    Console.WriteLine("нет");
