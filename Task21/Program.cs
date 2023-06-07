d7783a9eb06daac6de33df6bb3504abd85065963
Julia <reboot.3107@gmail.com>
2023-06-07 23:55:04 +0300

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите координату X1: ");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y1: ");
+double y1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Z1: ");
+double z1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату X2: ");
+double x2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y2: ");
+double y2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Z2: ");
+double z2 = double.Parse(Console.ReadLine()!);
+double S = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
+Console.WriteLine($"Расстояние равно {Math.Round(S, 3)}");
